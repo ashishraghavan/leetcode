71fd90b245ac6500eb2136ef71b68b2c889d75d4
Ashish Raghavan <ashishraghavan13687@gmail.com>
2019-09-28 20:42:04 +0100

    Solution to easy problem Defanging IP address - accepts only valid IPV4 addresses.

---
+++ src/com/leetcode/easy/DefangingIPAddress.java
@@ -0,0 +1,25 @@
+package com.leetcode.easy;
+
+import java.util.regex.Pattern;
+
+public class DefangingIPAddress {
+
+    public static void main(String[] args){
+        String ipAddress = "1.1.1.1";
+        String invalidIpAddress = "192.168.0.";//192..168.0.0
+        System.out.println(defangIPaddr(ipAddress));
+    }
+
+    static String defangIPaddr(String address) {
+        String[] splitStr = address.split("\\.");
+        if(splitStr.length != 4) {
+            return "Not a valid IPV4 address";
+        }
+        StringBuilder stringBuilder = new StringBuilder(splitStr[0]);
+        for(int i = 1;i<splitStr.length - 1;i++) {
+            stringBuilder.append("[.]").append(splitStr[i]);
+        }
+        stringBuilder.append("[.]").append(splitStr[splitStr.length - 1]);
+        return stringBuilder.toString();
+    }
+}
