29ab91a54461514bddf566a5d76ff27ac34b1c67
Ashish Raghavan <ashishraghavan13687@hotmail.com>
2019-10-01 18:59:59 -0500

    Unsolved completely solution for Non Decreasing array leetcode. Check the TODO message for description of test case for which this is not solved.

---
+++ src/com/leetcode/easy/NonDecreasingArray.java
@@ -0,0 +1,38 @@
+package com.leetcode.easy;
+
+/**
+ * Given an array with n integers, your task is to check if it could become non-decreasing by modifying at most 1 element.
+ * <p>
+ * We define an array is non-decreasing if array[i] <= array[i + 1] holds for every i (1 <= i < n).
+ * <p>
+ * Example 1:
+ * Input: [4,2,3]
+ * Output: True
+ * Explanation: You could modify the first 4 to 1 to get a non-decreasing array.
+ * <p>
+ * Example 2:
+ * Input: [4,2,1]
+ * Output: False
+ * Explanation: You can't get a non-decreasing array by modify at most one element.
+ * The n belongs to [1, 10,000].
+ * TODO : Not solved. Test case for which this is not solved is {3,4,2,3}
+ */
+public class NonDecreasingArray {
+    public static void main(String[] args) {
+        //3,4,2,3
+        //4,2,1
+        //
+        System.out.println(checkPossibility(new int[]{4, 2, 3}));
+    }
+
+    private static boolean checkPossibility(int[] nums) {
+        int nonDecreasingCount = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if ((i + 1 < nums.length) && (nums[i + 1] >= nums[i])) {
+                nonDecreasingCount++;
+            }
+        }
+        return (nonDecreasingCount == nums.length ||
+                (nonDecreasingCount == nums.length - 1));
+    }
+}
