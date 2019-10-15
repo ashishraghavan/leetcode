7206b7981af862d41685c9e743041af03abe4235
Ashish <ashishraghavan13687@hotmail.com>
2019-10-15 04:58:51 +0100

    Add relevant question with possible solution to ReOrderLogFileData class.

--- src/com/leetcode/easy/strings/ReOrderLogFileData.java
+++ src/com/leetcode/easy/strings/ReOrderLogFileData.java
@@ -2,6 +2,26 @@ package com.leetcode.easy.strings;
 
 import java.util.*;
 
+/**
+ * You have an array of logs.  Each log is a space delimited string of words.
+ *
+ * For each log, the first word in each log is an alphanumeric identifier.  Then, either:
+ *
+ * Each word after the identifier will consist only of lowercase letters, or;
+ * Each word after the identifier will consist only of digits.
+ * We will call these two varieties of logs letter-logs and digit-logs.  It is guaranteed that each log has at least one word after its identifier.
+ *
+ * Reorder the logs so that all of the letter-logs come before any digit-log.  The letter-logs are ordered lexicographically ignoring identifier, with the identifier used in case of ties.  The digit-logs should be put in their original order.
+ *
+ * Return the final order of the logs.
+ *
+ * USE COMPARATOR CLASS : Comparator strComparator = new Comparator<String> {
+ *      @Override
+ *      public int compare(String s1,String s2) {
+ *
+ *      }
+ * }
+ */
 public class ReOrderLogFileData {
     public static void main(String[] args) {
         //"let1 art can"
