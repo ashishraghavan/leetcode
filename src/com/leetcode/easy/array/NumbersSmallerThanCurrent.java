50c2d81bd25bd2fcb9172b57d1415ef312fd139a
ashishraghavan <ashishraghavan13687@hotmail.com>
2020-03-11 21:17:10 -0500

    Solution to leetcode easy array problem Numbers smaller than current - 1365

--- src/com/leetcode/easy/array/NumbersSmallerThanCurrent.java
+++ src/com/leetcode/easy/array/NumbersSmallerThanCurrent.java
@@ -4,13 +4,25 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-//TODO : Yet to be solved!! It's an easy problem.
+/**
+ * Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
+ * That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+ * Return the answer in an array.
+ * Constraints:
+ *
+ * 2 <= nums.length <= 500
+ * 0 <= nums[i] <= 100
+ */
 public class NumbersSmallerThanCurrent {
     public static void main(String[] args) {
         //8,1,2,2,3
         //7,7,7,7
         //6,5,4,8
-        System.out.println(Arrays.toString(smallerNumbersThanCurrent(new int[]{8,1,2,2,3})));
+        System.out.println(Arrays.toString(smallerNumbersThanCurrent(new int[]{7,7,7,7})));
+        //after sorting
+        //1,2,2,8,3 - Set<Integer>.add(1) - on 2 -> position will be the count
+        //1,2,2,8,3 - Map<Integer,Integer> map = 1 -> check to see if map contains 1. If it does, continue
+        //                                       2 -> check to see if map contains 2. If not
     }
 
     //8,1,2,2,3
@@ -18,24 +30,15 @@ public class NumbersSmallerThanCurrent {
     static int[] smallerNumbersThanCurrent(int[] nums) {
         int[] numCopy = Arrays.copyOf(nums,nums.length);
         Arrays.sort(numCopy);
-        Map<Integer,Integer> countMap = new HashMap<>();
+        Map<Integer,Integer> map = new HashMap<>();
         for(int i=0;i<numCopy.length;i++) {
-            if(!countMap.containsKey(numCopy[i])) {
-                countMap.put(numCopy[i],1);
-            } else {
-                countMap.put(numCopy[i],countMap.get(numCopy[i]) + 1);
+            if(map.containsKey(numCopy[i])) {
+                continue;
             }
+            map.put(numCopy[i],i);
         }
-
-        int j = numCopy.length - 1;
-        while(j>=0) {
-            int count = countMap.get(numCopy[j]);
-            int temp = j;
-            j = Math.abs(j-count);
-            countMap.put(numCopy[temp],j+1);
-        }
-        for(int k=0;k<nums.length;k++) {
-            nums[k] = countMap.get(nums[k]);
+        for(int i=0;i<nums.length;i++) {
+            nums[i] = map.get(nums[i]);
         }
         return nums;
     }
