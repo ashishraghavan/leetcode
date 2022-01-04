4dc23827233bad31997a3c17317ed0140f5daa50
ashishraghavan <ashish13687@icloud.com>
2022-01-05 03:23:49 +0530

    Improve run time for NumberComplement.java marginally.

--- src/com/leetcode/bitmanipulation/NumberComplement.java
+++ src/com/leetcode/bitmanipulation/NumberComplement.java
@@ -8,13 +8,15 @@ package com.leetcode.bitmanipulation;
  * Given an integer n, return its complement.
  *
  * 101 - xor bits? 1 xo 0 = 1 xo 1 = 0
+ * 1010 => 0101 = 0 + (2^2) + 0 + (2^0) = 4 + 1 = 5
  */
 public class NumberComplement {
     public static void main(String[] args) {
         int n = 10;
-        System.out.println(bitwiseComplement(n));
+        System.out.println(betterBitwiseComplement(n));
     }
 
+    @SuppressWarnings("unused")
     static int bitwiseComplement(int n) {
         if(n == 1)return 0;
         if(n == 0)return 1;
@@ -31,5 +33,30 @@ public class NumberComplement {
         }
         binStr = String.valueOf(binChArr);
         return Integer.parseInt(binStr,2);
+    }
+
+    /**
+     * 1. convert to binary string using built-in Integer.toBinaryString() method.
+     * 2. traverse in reverse direction - start from last character of string from step 1.
+     * 3. if this character is a 1, we are not interested in it because it will be changed to a 0 for 1st complement
+     * 4. if this character is a 0, we do a Math.pow(2,x), where x = absolute value of (index of i + 1) - (binary string length)
+     *      -- we do this to convert the binary string to decimal while we flip the bits. Also we are interested in
+     *         only bits which are 0 because we will be flipping these to 1. The ones that are 1 will be flipped to 0
+     *         & thus not being a part of the conversion to decimal.
+     */
+    static int betterBitwiseComplement(int n) {
+        if(n == 1)return 0;
+        if(n == 0)return 1;
+        //n = 5
+        String binStr = Integer.toBinaryString(n);
+        //binstr = 101
+        int bitVal = 0;
+        int pow;
+        for(int i=binStr.length()-1;i>=0;i--) {
+            if(binStr.charAt(i) == '1')continue;
+            pow = Math.abs((i+1) - binStr.length());
+            bitVal += Math.pow(2,pow);
+        }
+        return bitVal;
     }
 }
