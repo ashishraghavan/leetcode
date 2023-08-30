1a06d67264607ebbdbdc49a0ca004758134253ef
ashishraghavan <ashish13687@icloud.com>
2023-08-30 15:42:10 +0530

    Problem to calculate max sum sub-array of size k based on theory of fixed size sliding window

---
+++ src/com/leetcode/practice/FixedSizeSlidingWindow.java
@@ -0,0 +1,30 @@
+package com.leetcode.practice;
+
+/**
+ * Patterns in problem solving
+ * Sliding window pattern
+ *      Find max sum of k consecutive numbers in array of size n
+ */
+public class FixedSizeSlidingWindow {
+    public static void main(String[] args) {
+        //n = 5, k = 3
+        int[] arr = new int[]{-7, -1, 17, -9, 5, -12, 18, 15, -4, 7};
+        int k = 3;
+        //int answer = Arrays.stream(arr).sum();
+        //int maxSum = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0;i < k; i++) {
+            sum += arr[i];
+        }
+        //sum = 9
+        //maxSum = 9
+        int maxSum = sum;
+        int slidWinSum;
+        for (int i = k; i < arr.length; i++) {
+            slidWinSum = sum + arr[i] - arr[i - k];
+            sum = slidWinSum;
+            maxSum = Math.max(maxSum, slidWinSum);
+        }
+        System.out.println("max sum in sub-array of size "+k+" is "+maxSum);
+    }
+}
