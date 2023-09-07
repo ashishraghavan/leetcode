b85894e35de4e7a687897339b73b596dd07bb6dd
ashishraghavan <ashish13687@icloud.com>
2023-09-07 18:50:11 +0530

    update to RemoveDuplicatesFromSortedArray_II.java - moving all -1's to end of array & returning length of array = idx(-1) - 1, does not take care of edge cases.

--- src/com/leetcode/array/RemoveDuplicatesFromSortedArray_II.java
+++ src/com/leetcode/array/RemoveDuplicatesFromSortedArray_II.java
@@ -8,6 +8,7 @@ public class RemoveDuplicatesFromSortedArray_II {
         //arr has been sorted, time comp. = O(nlogn) using quick sort
         int[] A = new int[]{1,1,1,2,2,2,2,4,4,4};
         int count = 1, i = 1, curr = A[0];
+        //O(n)
         while ( i < A.length ) {
             if ( A[i] == curr ) {
                 count++;
@@ -21,5 +22,34 @@ public class RemoveDuplicatesFromSortedArray_II {
             i++;
         }
         System.out.println(Arrays.toString(A));
+        int a = -1, b;
+        //O(n)
+        for (int k = 0; k < A.length; k++) {
+            if (A[k] == -1) {
+                a = k;
+                break;
+            }
+        }
+        if ( a == -1 ) {
+            //no element = -1 in A
+            return;
+        }
+        b = a + 1;
+        //O(n) -- ??
+        while ( a < b && b < A.length) {
+            if (A[b] != -1) {
+                swap(A, a, b);
+                a++;
+            }
+            b++;
+        }
+        System.out.println(Arrays.toString(A));
+        System.out.println("length : "+a);
+    }
+
+    private static void swap(int[] A, int idx1, int idx2) {
+        int temp = A[idx1];
+        A[idx1] = A[idx2];
+        A[idx2] = temp;
     }
 }
