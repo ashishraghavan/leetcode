c79c818eee6342a430ae4f0e5ea50e6fd50aadee
ashishraghavan <ashish13687@icloud.com>
2023-09-10 17:10:58 +0530

    Rename RemoveDuplicateElementsFromArray -> RemoveElementFromArray, add problem source in comments, take care of case when element to delete is not present in the array.

--- src/com/leetcode/array/RemoveElementFromArray.java
+++ src/com/leetcode/array/RemoveElementFromArray.java
@@ -1,8 +1,12 @@
 package com.leetcode.array;
 
 import java.util.Arrays;
+import java.util.OptionalInt;
 
 /**
+ *
+ * interviewbit.com - see Two Pointers Problems
+ * leetcode - #27
  * Given A : [4,1,4,1,4,2] & B = some value in the array,
  * remove all instances of B from A.
  * update the array so that all instances of B
@@ -10,12 +14,20 @@ import java.util.Arrays;
  * return the length of the updated array - 3 in this case.
  * What remains after you remove all duplicates from the array does not matter
  */
-public class RemoveDuplicateElementsFromArray {
+public class RemoveElementFromArray {
     public static void main(String[] args) {
         //int[] A = new int[]{4,1,4,1,4,2};//B = 4
         //int[] A = new int[]{1, 6, 8, 10, 0, 3, 10, 3};//B = 3
         int[] A = new int[]{7, 2, 5, 3, 6, 8, 6, 9};//B = 6
-        int i = 0, j = 0, B = 6;
+        int i = 0, j = 0, B = 10;
+        //if B doesn't exist in A, print array & length = A.length
+        //time comp. === O(n), space comp. === O(1)
+        if (!Arrays.stream(A).filter(element -> element == B).findFirst().isPresent()) {
+            System.out.println(Arrays.toString(A));
+            System.out.println("length : "+(A.length));
+            return;
+        }
+        //
         while ( j < A.length ) {
             if ( A[j] == B ) {
                 j++;
