49960ead617321defdddabb6b5817cbadd8e7168
ashishraghavan <ashish13687@icloud.com>
2023-11-03 11:55:50 +0530

    Update code for LastNodeOfCompleteBinaryTree.java to cover a couple of edge cases.

--- src/com/leetcode/practice/LastNodeOfCompleteBinaryTree.java
+++ src/com/leetcode/practice/LastNodeOfCompleteBinaryTree.java
@@ -3,9 +3,16 @@ package com.leetcode.practice;
 public class LastNodeOfCompleteBinaryTree {
     public static void main(String[] args) {
         //create bin tree - 1,2,3,4,5
+        //test case #1
         Node root = BinaryTreeTester.createBinaryTree(6, BinaryTree.DIRECTION.RIGHT);
-        int heightLeft = 1 + heightLeft(root.getLeft());
-        int heightRight = 1 + heightRight(root.getRight());
+        //test case #2 - with nodes 1, 2, 3
+        //test case #3 - with nodes 1, 2
+//        BinaryTree bTree = new BinaryTree(1);
+//        bTree.insertNode(2);
+//        bTree.insertNode(3);
+//        Node root = bTree.getRoot();
+        int heightLeft = root.getLeft() == null ? 0 : 1 + heightLeft(root.getLeft());
+        int heightRight = root.getRight() == null ? 0 : 1 + heightRight(root.getRight());
         System.out.println("ht of left subtree : "+heightLeft+", height of right subtree : "+heightRight);
         if (heightLeft == heightRight) {
             System.out.println(lastChildOnRight(root));
@@ -32,6 +39,10 @@ public class LastNodeOfCompleteBinaryTree {
 
     static int lastChildOnRight(Node node) {
         if (node.getRight() == null) {
+            //for the case when complete binary tree has nodes on all levels including leaf node level.
+            if (node.getLeft() == null) {
+                return node.getValue();
+            }
             return node.getLeft().getValue();
         } else {
             return lastChildOnRight(node.getRight());
