a9ee2340cc8327f383c1850c3619a04e10ca9e22
ashishraghavan <ashish13687@icloud.com>
2025-04-22 00:46:28 +0530

    Attempted solution to problem # 1426 - counting elements

---
+++ src/com/leetcode/premium/CountingElements_L1426.java
@@ -0,0 +1,35 @@
+package com.leetcode.premium;
+
+import java.util.Arrays;
+
+//problem # 1426
+//check https://github.com/doocs/leetcode/blob/main/solution/1400-1499/1426.Counting%20Elements/README_EN.md
+//for
+//more
+//details
+public class CountingElements_L1426 {
+    public static void main(String[] args) {
+        //1,1,3,3,5,5,7,7
+        //1,2,3
+        //2,0,7,9,8,3,10,5,4,2,1,3 = 10
+        System.out.println(countElements(new int[]{2,0,7,9,8,3,10,5,4,2,1,3}));
+    }
+
+    public static int countElements(int[] A) {
+        //sort
+        Arrays.sort(A);
+        int[] aCt = new int[A[A.length-1]+1]; //initialized with 0
+        for (int j : A) {
+            ++aCt[j];
+        }
+        int res = 0;
+        for(int k : A) {
+            if(k+1 < aCt.length && aCt[k+1] > 0) {
+                res++;
+            }
+        }
+        return res;
+//        System.out.println(Arrays.toString(aCt));
+//        return -1;
+    }
+}
