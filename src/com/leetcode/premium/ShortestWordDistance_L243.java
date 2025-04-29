835dac0ed61c361de552b359b96282b81d671174
ashishraghavan <ashish13687@icloud.com>
2025-04-29 23:02:45 +0530

    add solution using hashmap -- not passing all test cases -- to problem # 243 - shortest word distance

--- src/com/leetcode/premium/ShortestWordDistance_L243.java
+++ src/com/leetcode/premium/ShortestWordDistance_L243.java
@@ -1,13 +1,22 @@
 package com.leetcode.premium;
 
-//TODO: pending
+import java.util.HashMap;
+import java.util.Map;
+
+//TODO: review, IMPORTANT!
+//check https://github.com/doocs/leetcode/tree/main/solution/0200-0299/0243.Shortest%20Word%20Distance
+//for
+//more
+//details
 public class ShortestWordDistance_L243 {
     public static void main(String[] args) {
         //shortestWordDistance(new String[]{"practice", "makes", "perfect", "coding", "makes"},"coding","practice")
         //{"practice", "makes", "perfect", "coding", "makes"}, "makes","coding"
-        System.out.println(shortestWordDistanceBruteForce(new String[]{"practice", "makes", "perfect", "coding", "makes"}, "makes","coding"));
+        //{"practice", "ashish", "perfect", "coding", "minu","perfect","minu","perfect","ashish","minu"}, "ashish","minu"
+        System.out.println(shortestWordDistance(new String[]{"practice", "ashish", "perfect", "coding", "minu","perfect","minu","perfect","ashish","minu"}, "ashish","minu"));
     }
 
+    //incorrect solution!!! doesn't work for all test cases
     public static int shortestWordDistance(String[] W,String w1,String w2) {
         //i = 0, j = W.length-1
         //if W[i].equals(w1) && W[j].equals(w2) || W[i].equals(w2) && W[j].equals(w1) -> dist = abs(j-i+1)
@@ -20,15 +29,51 @@ public class ShortestWordDistance_L243 {
         //{-1,1,-1,2,1}
 
         //map = {practice:0,makes:1,perfect:2,coding:3,makes:4}
-        //for each word in W, if not in map, map.put(word,idx)
+        //{makes:1}
+        //{makes:1,}
+        //{makes:1,coding:3}
+        //{makes:1,coding:3}
+        //for each word in W
+        //          if(!word.equals(w1) && !word.equals(w2)) continue;
+        //          if word not in map {
+        //              map.put(word,indexOf(word))
+        //          }
         //          String curr = word;
         //          other = word.equals(w1) ? w2:w1;
         //          if(map.contains(other)) {
-        //              dist = min(dist,abs(map.get(other)-map.get(curr)));
+        //              int curDist = abs(map.get(other)-map.get(curr);
+        //              if curDist < dist {
+        //                  dist = curDist;
+        //                  //update
+        //                  map.put(curr,indexOf(curr));
+        //              }
         //          }
-        return -1;
+
+        //"practice", "makes", "perfect", "coding", "makes"}, "makes","coding"
+        //{makes:1,coding:3}
+        Map<String,Integer> map = new HashMap<>();
+        int curDist,dist = Integer.MAX_VALUE;
+        for(int i=0;i<W.length;i++) {
+            if (!W[i].equals(w1) && !W[i].equals(w2)) {
+                continue;
+            }
+            if (!map.containsKey(W[i])) {
+                map.put(W[i],i);
+            }
+            String curr = W[i];
+            String other = curr.equals(w1) ? w2 : w1;
+            if(map.containsKey(other)) {
+                curDist = Math.abs(map.get(other) - i);
+                if(curDist < dist) {
+                    dist = curDist;
+                    map.put(curr,i);
+                }
+            }
+        }
+        return dist;
     }
 
+    //inefficient
     public static int shortestWordDistanceBruteForce(String[] W,String w1, String w2) {
         int dist = Integer.MAX_VALUE;
         for(int i=0;i<W.length;i++) {
