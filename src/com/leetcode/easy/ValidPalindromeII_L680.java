73f4f8dc1c8ef996fc813cb9a65a68db4f91977e
ashishraghavan <ashish13687@icloud.com>
2025-05-19 00:08:00 +0530

    added tag todo pending to problem # 680 - valid palindrome II

---
+++ src/com/leetcode/easy/ValidPalindromeII_L680.java
@@ -0,0 +1,57 @@
+package com.leetcode.easy;
+
+//problem # 680 - valid palindrome II
+//TODO: pending - revisit problem
+public class ValidPalindromeII_L680 {
+    public static void main(String[] args) {
+        //deeee
+        //deeeedd
+        //aba
+        //abca
+        //abc
+        //dcqabaqcd
+        //dqcababqd -> dqababqd || dqcabaqd
+        //ashsa
+        //ashisa
+        //adshsca
+        //System.out.println(validPalindrome("deeee"));
+//        char[] abh = new char[26];
+//        String alphabets = "abcdefghijklmnopqrstuvwxyz";
+//        for(int i=0;i<alphabets.length();i++) {
+//            abh[i] = alphabets.charAt(i);
+//        }
+//        for(int i=0;i<abh.length;i++) {
+//            System.out.println(i + " = "+abh[i]);
+//        }
+    }
+
+    //
+    //atmost 1 => you can also remove zero characters i.e. string is already a palindrome
+    //if isPalindrome(s) => return true
+    //pointer f=0, l = s.length()-1, int diff = 0;
+    //while(f<l) {
+    //  if(s.charAt(f) != s.charAt(l)) break;
+    // }
+    // return (l-f == 1)
+    public static boolean validPalindrome(String s) {
+        if(s.length() == 1)return true;
+        int[] bets = new int[26];
+        //deeee -> bets[3] = 1, bets[4] = 4
+        //ashisa -> bets[0], bets[]
+        int f = 0, l = s.length()-1;
+        while(f < l) {
+            if(s.charAt(f) != s.charAt(l))break;
+            f++;
+            l--;
+        }
+        return f >= l || l-f == 1;
+    }
+
+    public static boolean validPalindromeII(String s) {
+
+    }
+
+    private static boolean isPalindrome(String s) {
+        return false;
+    }
+}
