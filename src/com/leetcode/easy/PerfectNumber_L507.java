03bf10dbb5af8bc42d014afbc82d65171ac02f6a
ashishraghavan <ashish13687@icloud.com>
2025-05-20 01:22:04 +0530

    Inefficient solution to problem # 507 - perfect number, added github solution link

---
+++ src/com/leetcode/easy/PerfectNumber_L507.java
@@ -0,0 +1,32 @@
+package com.leetcode.easy;
+
+//problem # 507 - perfect number
+//check https://github.com/doocs/leetcode/tree/main/solution/0500-0599/0507.Perfect%20Number for github solution
+public class PerfectNumber_L507 {
+    public static void main(String[] args) {
+        //6
+        //7
+        //28
+        //496
+        //500
+        //8128
+        //8179
+        //33550336
+        //33550379
+        //100000000
+        System.out.println(checkPerfectNumber(11));
+    }
+
+    //inefficient solution
+    //for i in 1 to 28/2 if num%i == 0 && res < num {res+=i}
+    public static boolean checkPerfectNumber(int num) {
+        int res = 0;
+        for(int i=1;i<=num/2;i++) {
+            if(res > num)return false;
+            if(num%i == 0) {
+                res+=i;
+            }
+        }
+        return res==num;
+    }
+}
