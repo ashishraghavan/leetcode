393490daf65c1116e9cb7ee0f91dc82eb713c078
ashishraghavan <ashish13687@icloud.com>
2025-05-29 22:35:29 +0530

    Add github solution link & solution method for problem # 278

--- src/com/leetcode/easy/FirstBadVersion_L278.java
+++ src/com/leetcode/easy/FirstBadVersion_L278.java
@@ -1,13 +1,14 @@
 package com.leetcode.easy;
 
 //problem # 278 - first bad version
-//TODO: revise, pending checking github solution
+//TODO: revise
+//check https://github.com/doocs/leetcode/tree/main/solution/0200-0299/0278.First%20Bad%20Version for github solution
 public class FirstBadVersion_L278 {
     public static void main(String[] args) {
         //1,2,3,4,5 | bad = 4
         //1,2,3,4,5,6,7,8 | bad = 7
         //1....289786902
-        System.out.println(firstBadVersion(289786902));
+        System.out.println(firstBadVersionII(10));
     }
 
     //1,2,3,4,5 | bad = 4
@@ -42,9 +43,23 @@ public class FirstBadVersion_L278 {
         return -1;
     }
 
+    //method in github solution
+    public static int firstBadVersionII(int n) {
+        int start = 1, end = n, mid;
+        while(start < end) {
+            mid = (end+start)>>1;
+            if(isBadVersion(mid)) {
+                end = mid;
+            } else {
+                start = mid+1;
+            }
+        }
+        return start;
+    }
+
     public static boolean isBadVersion(int idx) {
         //return idx >= 144993451;
         //return idx >= 289786901;
-        return idx >= 289786900;
+        return idx >= 2;
     }
 }
