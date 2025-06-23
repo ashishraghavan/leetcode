d834acc5b66e4de6694da48b4b00aa22a14d70b9
ashishraghavan <ashish13687@icloud.com>
2025-06-24 00:59:09 +0530

    Equals, hashcode & toString methods for ListNode.java

--- src/com/leetcode/linkedlist/ListNode.java
+++ src/com/leetcode/linkedlist/ListNode.java
@@ -1,16 +1,38 @@
 package com.leetcode.linkedlist;
 
+import java.util.Objects;
+
 /**
  * Class definition provided by leetcode
  */
 public class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) {
+    public int val;
+    public ListNode next;
+    public ListNode() {}
+    public ListNode(int val) {
         this.val = val;
     }
-    ListNode(int val, ListNode next) {
+    public ListNode(int val, ListNode next) {
         this.val = val; this.next = next;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ListNode listNode = (ListNode) o;
+        return val == listNode.val && Objects.equals(next, listNode.next);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = val;
+        result = 31 * result + Objects.hashCode(next);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return val+ " -> ";
     }
 }
