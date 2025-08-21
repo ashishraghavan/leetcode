6e500c000e4b7fd41d304596964471623b09ec5c
ashishraghavan <ashish13687@icloud.com>
2025-08-21 23:33:46 +0530

    Non-optimized solution to prob # 3083 - existence of substring in a string and its reverse

---
+++ src/com/leetcode/easy/ExistenceOfSubstringInAStringAndItsReverse_L3083.java
@@ -0,0 +1,30 @@
+package com.leetcode.easy;
+
+//prob # 3083 - existence of substring in a string and its reverse
+public class ExistenceOfSubstringInAStringAndItsReverse_L3083 {
+    public static void main(String[] args) {
+        //leetcode
+        //abcba
+        //abcd
+        //leafbcaef
+        System.out.println(isSubstringPresent("leafbcaef"));
+    }
+
+    //leetcode
+    //[0.0.1.1.3....1...........1....]
+    //edocteel
+    //abcba
+    //abcba
+    //[2,2,1]
+    //dzfbgzdfb
+    public static boolean isSubstringPresent(String s) {
+        StringBuilder sb = new StringBuilder(2);
+        for(int i=1;i<s.length();i++) {
+            sb.setLength(0);
+            if(s.charAt(i) == s.charAt(i-1))return true;
+            sb.append(s.charAt(i)).append(s.charAt(i-1));
+            if(s.indexOf(sb.toString(),i) > 0)return true;
+        }
+        return false;
+    }
+}
