221d1ffe1b706b8aaddc50c91f66e740911827bf
Ashish Raghavan <ashish13687@icloud.com>
2025-09-30 00:16:16 +0530

    Allow constructor with single argument - value of node, default left & right children as null

--- src/com/leetcode/tree/core/TreeNode.java
+++ src/com/leetcode/tree/core/TreeNode.java
@@ -5,7 +5,11 @@ public class TreeNode {
     TreeNode left;
     TreeNode right;
     TreeNode() {}
-    public TreeNode(int val) { this.val = val; }
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
     public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
