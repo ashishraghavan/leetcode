39134dbf1c6fbd28f126a530ce78d3442407b4fe
Ashish Raghavan <ashish13687@icloud.com>
2025-10-30 00:05:18 +0530

    Added solution link for prob #1769 - min operations to move all balls to each box

--- src/com/leetcode/medium/array_strings/MinOpsToMoveAllBallsToEachBox_L1769.java
+++ src/com/leetcode/medium/array_strings/MinOpsToMoveAllBallsToEachBox_L1769.java
@@ -4,13 +4,14 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-//TODO: definitely need to optimize
+//TODO: check https://algo.monster/liteproblems/1769 for intuitive solution involving dynamic programming
 //prob #1769 - min operations to move all balls to each box
 public class MinOpsToMoveAllBallsToEachBox_L1769 {
     public static void main(String[] args) {
         //001011
         //110
-        System.out.println(Arrays.toString(minOperations("001011")));
+        //10101011
+        System.out.println(Arrays.toString(minOperationsII("10101011")));
     }
 
     //001011
@@ -36,6 +37,38 @@ public class MinOpsToMoveAllBallsToEachBox_L1769 {
                 sum += (Math.abs(key - i));
             }
             res[i] = sum;
+        }
+        return res;
+    }
+
+    //10101011
+    //for each i in (1,7) cost at i = cost at i-1 + total_balls_to_left_of_i
+    //i=0 -> 0 + 0 => left[0] = 0,
+    //i=1 -> boxes[i-1] == 1 ? balls++,
+    //i=3, cost[2] + balls -> cost[2] = cost[1] + balls -> cost[2] = cost[1]+balls ->
+    // cost[1] = cost[0] + balls ==> at idx=0,balls to left of idx=0 & cost[0] = 0 as
+    // there are no balls to be moved to left of idx
+    //i=3,balls=0; cost[7] = cost[8] + balls = 0, cost[6] ->  boxes[7]==1 ? balls++ = 1 => cost[6] = 0+1 = 1
+
+    ////check [...](https://algo.monster/liteproblems/1769) for intuitive solution
+    public static int[] minOperationsII(String boxes) {
+        int[] left = new int[boxes.length()];
+        int[] right = new int[boxes.length()];
+        int[] res = new int[boxes.length()];
+        int balls = 0;
+        for(int i=1;i<left.length;i++) {
+            if(boxes.charAt(i-1) == '1')balls++;
+            left[i] = left[i-1] + balls;
+        }
+        System.out.println(Arrays.toString(left));
+        balls = 0;
+        for(int i=right.length-2;i>=0;i--) {
+            if(boxes.charAt(i+1) == '1')balls++;
+            right[i] = right[i+1] + balls;
+        }
+        System.out.println(Arrays.toString(right));
+        for(int i=0;i<boxes.length();i++) {
+            res[i] = left[i] + right[i];
         }
         return res;
     }
