6ef29b3e2459659a8087edc501b41ec7515fdbc9
Ashish Raghavan <ashish13687@icloud.com>
2025-11-01 23:31:48 +0530

    Add link to solution with T.C=O(n), S.C=O(1) for prob #238 - product of array except self

--- src/com/leetcode/medium/array_strings/ProductOfArrayExceptSelf_L238.java
+++ src/com/leetcode/medium/array_strings/ProductOfArrayExceptSelf_L238.java
@@ -2,11 +2,13 @@ package com.leetcode.medium.array_strings;
 
 import java.util.Arrays;
 
+//prob #238 - product of array except self
+//TODO: refer https://copilot.microsoft.com/shares/Sa1mL7GxYmL6SEn3mbDVt for explanation of solution - productExceptSelfII(..)
 public class ProductOfArrayExceptSelf_L238 {
     public static void main(String[] args) {
         //1,2,3,4
         //-1,1,0,-3,3
-        System.out.println(Arrays.toString(productExceptSelf(new int[]{-1,1,0,-3,3})));
+        System.out.println(Arrays.toString(productExceptSelfII(new int[]{1,2,3,4})));
     }
 
     //1,2,3,4
@@ -34,6 +36,25 @@ public class ProductOfArrayExceptSelf_L238 {
             res[i] *= left[i];
         }
         //System.out.println(Arrays.toString(res));
+        return res;
+    }
+
+    //1,2,3,4
+    //24,12,4,1
+    //1,1,2,6
+    //product=res[0], i in nums, res[i]*=product,product*=nums[i]
+    public static int[] productExceptSelfII(int[] nums) {
+        int[] res = new int[nums.length];
+        int product=1;
+        for(int i=res.length-1;i>=0;i--) {
+            res[i]=product;
+            product*=nums[i];
+        }
+        product=1;
+        for(int i=0;i<nums.length;i++) {
+            res[i]*=product;
+            product*=nums[i];
+        }
         return res;
     }
 }
