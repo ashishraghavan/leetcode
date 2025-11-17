8eb376faa5e55e58be2a1921044989663eeb271c
Ashish Raghavan <ashish13687@icloud.com>
2025-11-18 00:01:02 +0530

    Add solution as per co-pilot using bucketsort for problem #451 - sort characters by frequency

--- src/com/leetcode/medium/array_strings/SortCharactersByFrequency_L451.java
+++ src/com/leetcode/medium/array_strings/SortCharactersByFrequency_L451.java
@@ -2,14 +2,15 @@ package com.leetcode.medium.array_strings;
 
 import java.util.*;
 
-//TODO: can also be done by bucket sort (see https://copilot.microsoft.com/shares/tDFGg4R4Jch752HnZtQK9)
+//TODO: can also be done by bucket sort (see https://copilot.microsoft.com/shares/tDFGg4R4Jch752HnZtQK9),
+// see method frequencySortII(..) on line #45 - demonstrates bucket sort!!
 //problem #451 - sort characters by frequency
 public class SortCharactersByFrequency_L451 {
     public static void main(String[] args) {
         //tree
         //cccaaa
         //Aabb
-        System.out.println(frequencySort("Aabb"));
+        System.out.println(frequencySortII("Aabb"));
     }
 
     //cccaaa
@@ -34,6 +35,38 @@ public class SortCharactersByFrequency_L451 {
             Map.Entry<Character,Integer> entry = pq.poll();
             //int times = entry.getValue();
             sb.append(String.valueOf(entry.getKey()).repeat(entry.getValue()));
+        }
+        System.out.println(sb);
+        return sb.toString();
+        //return "";
+    }
+
+    //running time still the same/even worse than earlier, maybe check co-pilot link above for differences
+    public static String frequencySortII(String s) {
+        Map<Character,Integer> mp = new HashMap<>();
+        for(int i=0;i<s.length();i++) {
+            //mp.merge(s.charAt(i),1,Integer::sum);
+            mp.put(s.charAt(i),mp.getOrDefault(s.charAt(i),0)+1);
+        }
+        System.out.println(mp);
+        //{a=1, A=1, b=2}
+        List<Character>[] buckets = new List[s.length()+1];
+        for(int i=0;i<=s.length();i++) {
+            buckets[i] = new ArrayList<>();
+        }
+        //output of below: [[], [A, a], [b], [], []]
+        for(Map.Entry<Character,Integer> entry : mp.entrySet()) {
+            buckets[entry.getValue()].add(entry.getKey());
+        }
+        //System.out.println(Arrays.toString(buckets));
+        StringBuilder sb = new StringBuilder();
+        for(int i=buckets.length-1;i>=0;i--) {
+            if(!buckets[i].isEmpty()) {
+                List<Character> chList = buckets[i];
+                for(Character ch : chList) {
+                    sb.append(String.valueOf(ch).repeat(i));
+                }
+            }
         }
         System.out.println(sb);
         return sb.toString();
