b69624dc1ff7be1c6e92a16991369d7d8ab4cea9
Ashish Raghavan <ashish13687@icloud.com>
2025-12-08 22:38:25 +0530

    Solution to algomonster problems - MiddleOfLinkedList, RemoveDuplicates

---
+++ src/com/algomonster/problems/twopointers/MiddleOfLinkedList.java
@@ -0,0 +1,57 @@
+package com.algomonster.problems.twopointers;
+
+public class MiddleOfLinkedList {
+    public static void main(String[] args) {
+        //0 1 2 3 4 5
+        //0 1 2 3 4
+        //new Node<>(5,null)
+        //Node<Integer> head = new Node<>(0,new Node<>(1,new Node<>(2,new Node<>(3,new Node<>(4,null)))));
+        //[1,3,5,7,9,11,13,15,17,19,21]
+        //[7,14,21,28,35,42,49,56,63,70,77,84]
+        //[11,22,33,44,55,66,77,88,99]
+        //1,2
+        System.out.println(middleOfLinkedList(buildList(new Integer[]{1})));
+    }
+
+    public static class Node<T> {
+        public T val;
+        public Node<T> next;
+
+        public Node(T val) {
+            this(val, null);
+        }
+
+        public Node(T val, Node<T> next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static int middleOfLinkedList(Node<Integer> head) {
+        if(head == null)return -1;
+        Node<Integer> fast = head;
+        Node<Integer> slow = head;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow != null ? slow.val : -1;
+    }
+
+    public static <T> Node<T> buildList(T[] arr) {
+        if (arr == null || arr.length == 0) {
+            return null; // empty list
+        }
+
+        Node<T> head = new Node<>(arr[0]);
+        Node<T> current = head;
+
+        for (int i = 1; i < arr.length; i++) {
+            current.next = new Node<>(arr[i]);
+            current = current.next;
+        }
+
+        return head;
+    }
+
+}
---
+++ src/com/algomonster/problems/twopointers/RemoveDuplicates.java
@@ -0,0 +1,27 @@
+package com.algomonster.problems.twopointers;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RemoveDuplicates {
+    //0, 0, 1, 1, 1, 2, 2
+    public static void main(String[] args) {
+        //[1, 1, 2, 3, 3, 4, 4, 5]
+        //[1, 2, 2, 3, 4, 4, 5, 6, 6]
+        System.out.println(removeDuplicates(Arrays.asList(1, 2, 2, 3, 4, 4, 5, 6, 6)));
+    }
+
+    //0, 0, 1, 1, 1, 2, 2
+    //f=0,s=1 -> f=1,s=2 -> 0,1,1,1,1,2,2 -> f=2,s=3,4,5
+    public static int removeDuplicates(List<Integer> arr) {
+        int f = 0,s = 0;
+        while(s < arr.size()) {
+            if(!arr.get(s).equals(arr.get(f))) {
+                f++;
+                arr.set(f,arr.get(s));
+            }
+            s++;
+        }
+        return f+1;
+    }
+}
