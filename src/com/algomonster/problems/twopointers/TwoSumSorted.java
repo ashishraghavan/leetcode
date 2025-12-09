0ac58c2a0063ebd50332a041675f29a4eb10c824
Ashish Raghavan <ashish13687@icloud.com>
2025-12-09 18:53:23 +0530

    Solution to problem - TwoSumSorted

---
+++ src/com/algomonster/problems/twopointers/TwoSumSorted.java
@@ -0,0 +1,26 @@
+package com.algomonster.problems.twopointers;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TwoSumSorted {
+    public static void main(String[] args) {
+        //2, 3, 4, 5, 8, 11, 18
+        //[2, 3, 4, 6, 8, 11]
+        System.out.println(twoSumSorted(Arrays.asList(2, 3, 4, 6, 8, 11),20));
+    }
+
+    public static List<Integer> twoSumSorted(List<Integer> arr, int target) {
+        int f = 0,l = arr.size()-1,sum;
+        while(f < l) {
+            sum = arr.get(f) + arr.get(l);
+            if(sum == target)return List.of(f,l);
+            else if(sum < target) {
+                f++;
+            } else {
+                l--;
+            }
+        }
+        return List.of();
+    }
+}
