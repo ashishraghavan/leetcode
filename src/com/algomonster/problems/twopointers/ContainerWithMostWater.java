5a3d3bcd42eb5ffb9a4725ed9244169e77978430
Ashish Raghavan <ashish13687@icloud.com>
2025-12-09 23:15:46 +0530

    Solution to problem ContainerWithMostWater

---
+++ src/com/algomonster/problems/twopointers/ContainerWithMostWater.java
@@ -0,0 +1,31 @@
+package com.algomonster.problems.twopointers;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ContainerWithMostWater {
+    public static void main(String[] args) {
+        //1,8,6,2,5,4,8,3,7
+        //[10,9,8,7,6,5,4,3,2,1]
+        //[1,2,3,4,3,2,1]
+        System.out.println(containerWithMostWater(Arrays.asList(1,2,3,4,3,2,1)));
+    }
+
+    //1,8,6,2,5,4,8,3,7
+    //[10,9,8,7,6,5,4,3,2,1]
+    //area = min(arr.get(f),arr.get(l)) * Math.abs(f-l)
+    //1,2,3,4,3,2,1
+    public static int containerWithMostWater(List<Integer> arr) {
+        int f = 0, l = arr.size() - 1;
+        int area = Integer.MIN_VALUE;
+        while(f < l) {
+            area = Math.max(area,Math.min(arr.get(f),arr.get(l)) * (l-f));
+            if(arr.get(f) <= arr.get(l)) {
+                f++;
+            } else {
+                l--;
+            }
+        }
+        return area;
+    }
+}
