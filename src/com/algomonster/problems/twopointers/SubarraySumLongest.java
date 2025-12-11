fe33f86c97824a352c99676d5e12fcbda4e08f0c
Ashish Raghavan <ashish13687@icloud.com>
2025-12-11 17:01:01 +0530

    Solution to problem SubarraySumLongest

---
+++ src/com/algomonster/problems/twopointers/SubarraySumLongest.java
@@ -0,0 +1,29 @@
+package com.algomonster.problems.twopointers;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SubarraySumLongest {
+    public static void main(String[] args) {
+        //1, 6, 3, 1, 2, 4, 5 | 10
+        System.out.println(subarraySumLongest(Arrays.asList(1, 6, 3, 1, 2, 4, 5),10));
+    }
+
+    //1, 6, 3, 1, 2, 4, 5 | 10
+    //f=0,s=1,cnt=0 sum=l[f]+l[s] == t ? cnt++; else if sum < t ? s++ else f++
+    public static int subarraySumLongest(List<Integer> l, int t) {
+        int maxLen = Integer.MIN_VALUE;
+        if(l.isEmpty())return 0;
+        int f=0,s,sum = 0;
+        for(s=0;s<l.size();s++) {
+            sum += l.get(s);
+            if(sum <= t){
+                maxLen = Math.max(maxLen,s-f+1);
+            } else {
+                sum -= l.get(f);
+                f++;
+            }
+        }
+        return maxLen;
+    }
+}
