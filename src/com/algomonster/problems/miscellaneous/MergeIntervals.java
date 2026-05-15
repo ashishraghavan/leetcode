21666193bb6c2d62ed0206509af2f5272326e78b
Ashish Raghavan <ashish13687@icloud.com>
2026-05-16 00:21:57 +0530

    Solution to miscellaneous problem Merge intervals with help from gemini

---
+++ src/com/algomonster/problems/miscellaneous/MergeIntervals.java
@@ -0,0 +1,45 @@
+package com.algomonster.problems.miscellaneous;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeIntervals {
+    public static void main(String[] args) {
+        //[[8,10],[2,6],[15,18],[1,3]]
+        //[[1,4],[2,3],[5,6]]
+        //Arrays.asList(Arrays.asList(8,10),Arrays.asList(2,6),Arrays.asList(15,18),Arrays.asList(1,3))
+        System.out.println(mergeIntervals(Arrays.asList(Arrays.asList(1,4),Arrays.asList(2,3),Arrays.asList(5,6))));
+    }
+
+    //[[1,3],[2,6],[8,10],[15,18]]
+    //sort by start time
+    //[[1, 3], [2, 6], [8, 10], [15, 18]]
+    //res = [[1,6],[8,10],[15,18]]
+    public static List<List<Integer>> mergeIntervals(List<List<Integer>> intervals) {
+        List<List<Integer>> res = new ArrayList<>();
+        intervals.sort((l1,l2)->l1.get(0).compareTo(l2.get(0)));
+        for(List<Integer> interval : intervals) {
+            if(res.isEmpty() || !overlap(res.get(res.size()-1),interval)) {
+                res.add(interval);
+            } else {
+                List<Integer> lastInterval = res.get(res.size()-1);
+                //max(interval.get(0),lastInterval.get(0)),min(interval.get(1),lastInterval.get(1))
+                if(overlap(interval,lastInterval)) {
+                    //set lastInterval y co-ordinate = y co-ordinate of interval
+                    //lastInterval.set(0,Math.min(lastInterval.get(0),interval.get(0)));
+                    lastInterval.set(1,Math.max(lastInterval.get(1),interval.get(1)));
+                } else {
+                    res.add(interval);
+                }
+            }
+        }
+        return res;
+    }
+
+    //[1, 3], [2, 6]
+    //max(1,2),min(3,6)
+    public static boolean overlap(List<Integer> l1,List<Integer> l2) {
+        return Math.max(l1.get(0),l2.get(0)) <= Math.min(l1.get(1),l2.get(1));
+    }
+}
